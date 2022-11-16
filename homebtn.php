b516a6f63acf1b33c5b7737023c4b567cc96900c
Heewon Lee <heewonlee@ewhain.net>
2022-11-16 14:40:35 +0900

    [fix] home button error fixed

--- homebtn.php
+++ homebtn.php
@@ -2,7 +2,7 @@
 header('Content-Type: text/html; charset=UTF-8');
 session_start();
 // 로그인 된 상태
-if(!isset($_SESSION['user'])) {
+if(isset($_SESSION['user'])) {
     echo "<script>location.replace('index.php');</script>";            
 }
 // 로그인 안된 상태
