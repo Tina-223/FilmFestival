e9706cc58fa4bbe1b40c6efc8c19f0809e18a6e3
Jeongmin <76679270+JeongminKim1@users.noreply.github.com>
2022-11-16 15:08:02 +0900

    Update country_result.php

--- country_result.php
+++ country_result.php
@@ -35,14 +35,22 @@ else{
 			$sql = "SELECT director.name as dname, film.name as fname, film_hit.sales as sales, rank() over (order by sales desc) as s_rank
 					FROM director, film, film_hit
 					WHERE director.nationality = '$ctr' and director.id = film.director_id and film.id = film_hit.film_id";
+			$sql2 = "SELECT AVG(film_hit.sales) as avgSales, SUM(film_hit.sales) as sumSales
+					FROM director, film, film_hit
+					WHERE director.nationality = '$ctr' and director.id = film.director_id and film.id = film_hit.film_id";
 		}
 		else{
 			$sql = "SELECT director.name as dname, film.name as fname, film_hit.sales as sales, rank() over (order by sales asc) as s_rank
 					FROM director, film, film_hit
 					WHERE director.nationality = '$ctr' and director.id = film.director_id and film.id = film_hit.film_id";
+			$sql2 = "SELECT AVG(film_hit.sales) as avgSales, SUM(film_hit.sales) as sumSales
+					FROM director, film, film_hit
+					WHERE director.nationality = '$ctr' and director.id = film.director_id and film.id = film_hit.film_id";
 		}
 		
 		$res = mysqli_query($mysqli, $sql);
+		$res2 = mysqli_query($mysqli, $sql2); //AVG, SUM
+
 		if($res){
 			echo "<table border cols = 4>\n";
 			echo "<tr><td> RANK </td><td> DIRECTOR NAME </td><td> FILM NAME </td><td> SALES </td></tr>\n";
@@ -52,6 +60,19 @@ else{
 				$fname = $newArray['fname'];
 				$sales = $newArray['sales'];
 				echo "<tr><td>".$srank."</td><td>".$dname."</td><td>".$fname."</td><td>".$sales."</td></tr>\n";
+			}
+			echo "</br>";
+		}else{
+			printf("Could not retrieve records: %s\n", mysqli_error($mysqli));
+		}
+
+		if($res2){
+			while($row = mysqli_fetch_array($res2, MYSQLI_ASSOC)){
+				echo "</br>";
+				echo "AVG of SALES : ".$row['avgSales'];
+				echo "</br>";
+				echo "SUM of SALES : ".$row['sumSales'];
+				echo "</br>";
 			}
 		}else{
 			printf("Could not retrieve records: %s\n", mysqli_error($mysqli));
