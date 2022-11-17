1de8aa6df747d5e68a6539f74914a4821e1558f4
Heewon Lee <heewonlee@ewhain.net>
2022-11-17 17:10:13 +0900

    [chore] test

--- write_review_academy.php
+++ write_review_academy.php
@@ -17,8 +17,19 @@ else {
         $choice=$_POST['choice']; #영화제 종류
         $film_name=$_POST['fname']; #영화이름
         $grade=$_POST['rating']; #평점
+        $gen=$_POST['gender'];
 
-        $id_final = 1;
+        $query_1 = "SELECT A.id
+        FROM academy_best_award A
+        JOIN film F ON A.film_id = F.id";
+        $result_1 = $mysqli->query($query_1);
+        if ($result_1) {
+            while($newarray_2 = mysqli_fetch_array($result_1, MYSQLI_ASSOC)){
+                $id_final = $newarray_2['id'];				
+                echo $id_final;
+            }
+        }
+
 
         if (isset($_POST['review'])) {
             $review = $_POST['review'];
@@ -30,28 +41,26 @@ else {
         $q = "INSERT INTO review_academy (grade, one_line_review, academy_award_id, user_id) values ('$grade', '$review', '$id_final', '$userid')";
         $res = mysqli_query($mysqli, $q);
 
+        $qu = "SELECT * FROM review_academy WHERE user_id = '$userid'";
+        $result = mysqli_query($mysqli, $qu);
+
         // insert문 실행 시
-        if ($res === TRUE) {
+        if ($res) {
             echo "<table border cols = 2>\n";
-            echo "<tr><td> ratings </td><td> revies </td></tr>\n";
-            while($array = mysqli_fetch_array($res, MYSQLI_ASSOC)){
-                $r_grade = $array['grade'];				
-                $r_review = $array['one_line_review'];
-                echo "<tr><td>".$r_grade."</td><td>".$r_review."</td></tr>\n";
+            echo "<tr><td> ratings </td></tr>\n";
+            while($newarray = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+                $r_grade = $newarray['grade'];				
+                echo "<tr><td>".$r_grade."</td></tr>\n";
             }
-        }else{
+        }
+        else {
             printf("Could not retrieve records: %s\n", mysqli_error($mysqli));
         }
 
 
         echo "<".$film_name.">"."에 대한 리뷰가 등록되었습니다.</br>";
-        echo "평점 :".$grade." 한줄평: ".$review;
+        echo "작성자: ".$userid.", 평점 :".$grade;
     }
 }
-
-
-// }
-
-
 
 ?>
