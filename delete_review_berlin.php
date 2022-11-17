b48f244d459364e109fd3c85ca2d40a87bc61d60
Chae Jieun <86587287+cje1903@users.noreply.github.com>
2022-11-17 23:11:03 +0900

    Create delete_review_berlin.php

---
+++ delete_review_berlin.php
@@ -0,0 +1,26 @@
+<?php
+
+header('Content-Type: text/html; charset=UTF-8');
+$userID = $_POST['userID'];
+
+session_start();
+$host = 'localhost';
+$user = 'team13';
+$pw = 'team13';
+$db_name = 'team13';
+$mysqli = new mysqli($host, $user, $pw, $db_name);
+
+if(mysqli_connect_errno()){
+	printf("Connect failed: %s\n", mysqli_error($mysqli));
+	exit();
+}
+else{
+
+}
+
+$sql = "DELETE FROM review_berlin WHERE user_id = $userID";
+$res = mysqli_query($mysqli, $sql);
+mysqli_close($mysqli);
+
+echo "<script>location.href='film_gender_grade.php'</script>";
+?>
