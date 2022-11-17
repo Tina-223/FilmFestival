ce53bbff3cff326e587258a4196dabe6e45bbd53
Jeongmin <76679270+JeongminKim1@users.noreply.github.com>
2022-11-17 22:10:48 +0900

    Create update_review_cannes.php

---
+++ update_review_cannes.php
@@ -0,0 +1,27 @@
+<?php
+
+header('Content-Type: text/html; charset=UTF-8');
+$userID = $_POST['userID'];
+$rating = $_POST['rating'];
+
+session_start();
+$host = 'localhost';
+$user = 'team13';
+$pw = 'team13';
+$db_name = 'team13';
+$mysqli = new mysqli($host, $user, $pw, $db_name);
+
+if(mysqli_connect_errno()){
+	printf("Connect failed: %s\n", mysqli_error($mysqli));
+	exit();
+}
+else{
+
+}
+
+$sql = "UPDATE review_cannes SET grade = $rating WHERE user_id = $userID";
+$res = mysqli_query($mysqli, $sql);
+mysqli_close($mysqli);
+
+echo "<script>location.href='film_gender_grade.php'</script>";
+?>
